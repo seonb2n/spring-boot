4b4aed12028e5b445696b1ccd836a7dd0a586c7e
seonb2n <626ksb@gmail.com>
2021-06-08 12:10:24 +0900

    commit test

--- src/main/java/com/example/demo/H2Runner.java
+++ src/main/java/com/example/demo/H2Runner.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 public class H2Runner implements ApplicationRunner {
 
     @Autowired
-    DataSource dataSource;
+    DataSource dataSource;    
 
     @Autowired
     JdbcTemplate jdbcTemplate;
