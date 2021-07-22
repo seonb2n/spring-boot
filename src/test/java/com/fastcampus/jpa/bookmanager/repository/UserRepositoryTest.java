676d49bd38672fe3bef469fa27f906852690528a
seonb2n <626ksb@gmail.com>
2021-07-22 21:19:07 +0900

    flush

--- src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,11 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.fastcampus.jpa.bookmanager.domain.User;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Martin
@@ -24,11 +29,10 @@ class UserRepositoryTest {
 
     @Test
     void crud() { // create, read, update, delete
-        userRepository.save(new User("david", "david@fastcampus.com"));
+        userRepository.save(new User("new Martin", "newMartin@naver.com"));
 
-        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
-        user.setEmail("martin-updated@fastcampus.com");
+        userRepository.flush();
 
-        userRepository.save(user);
+        userRepository.findAll().forEach(System.out::println);
     }
 }
