95d02c11369388f55ef2ba6dd5598e58b5d591b2
seonb2n <626ksb@gmail.com>
2021-08-19 16:11:09 +0900

    sql 파일 추가. ID GeneratedValue -> STRATEGY IDENTITY

--- src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
@@ -15,7 +15,8 @@ public class BookRepositoryTest {
     void bookTest() {
         Book book = new Book();
         book.setName("JPA LEARNING");
-        book.setAuthor("JPA MASTER");
+        book.setAuthorId(1L);
+        book.setPublisherId(1L);
 
         bookRepository.save(book);
 
