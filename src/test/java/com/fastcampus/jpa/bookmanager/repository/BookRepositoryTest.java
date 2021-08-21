7605b2f59206bc81c3859dbc8b6dfeaa43b99396
seonb2n <626ksb@gmail.com>
2021-08-21 17:56:23 +0900

    relation mapping test

--- src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
@@ -1,15 +1,29 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.Book;
+import com.fastcampus.jpa.bookmanager.domain.Publisher;
+import com.fastcampus.jpa.bookmanager.domain.Review;
+import com.fastcampus.jpa.bookmanager.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
 @SpringBootTest
 public class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void bookTest() {
@@ -23,7 +37,46 @@ public class BookRepositoryTest {
     }
 
     @Test
+    @Transactional
     void bookRelationTest() {
-        
+        givenBookAndReview();
+
+        User user = userRepository.findByEmail("martin@fastcampus.com");
+
+        System.out.println("Review : " + user.getReviews());
+        System.out.println("Book : " + user.getReviews().get(0).getBook());
+        System.out.println("Publisher : "+ user.getReviews().get(0).getBook().getPublisher());
+    }
+
+    private void givenBookAndReview() {
+        givenReview(givenUser(), givenBook(givenPublisher()));
+    }
+
+    private User givenUser() {
+        return userRepository.findByEmail("martin@fastcampus.com");
+    }
+
+    private Book givenBook(Publisher publisher) {
+        Book book = new Book();
+        book.setName("JPA Package");
+        book.setPublisher(publisher);
+        return bookRepository.save(book);
+    }
+
+    private Publisher givenPublisher() {
+        Publisher publisher = new Publisher();
+        publisher.setName("naver");
+        return publisherRepository.save(publisher);
+    }
+
+    private void givenReview(User user, Book book) {
+        Review review = new Review();
+        review.setTitle("my best book");
+        review.setContent("Very fun and good book");
+        review.setScore(5.0f);
+        review.setUser(user);
+        review.setBook(book);
+
+        reviewRepository.save(review);
     }
 }
