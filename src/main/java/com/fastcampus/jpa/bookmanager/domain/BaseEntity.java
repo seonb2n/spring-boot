5dbb22a93472730fbab002f579d9bf53b515b256
seonb2n <626ksb@gmail.com>
2021-08-30 20:26:17 +0900

    columnDefinition 으로 column 값의 default 설정하는 방법 학습

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/BaseEntity.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/BaseEntity.java
@@ -15,11 +15,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(value = AuditingEntityListener.class)
 public class BaseEntity implements Auditable {
     @CreatedDate
-    @Column(columnDefinition = "datetime(6) default now(6) comment='생성시간'",  nullable = false, updatable = false)
+    @Column(columnDefinition = "datetime(6) default now(6) comment '생성시간'",  nullable = false, updatable = false)
     private LocalDateTime createdAt;
     //columnDefinition 으로 해당 값의 default 값을 정할 수 있다.
 
     @LastModifiedDate
-    @Column(columnDefinition = "datetime(6) default now(6) comment='수정시간'", nullable = false)
+    @Column(columnDefinition = "datetime(6) default now(6) comment '수정시간'", nullable = false)
     private LocalDateTime updatedAt;
 }
