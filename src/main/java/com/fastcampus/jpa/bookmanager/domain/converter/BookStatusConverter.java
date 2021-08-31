8871dd7fa1d2d705554eaf4e081e736a3ba8d5d9
seonb2n <626ksb@gmail.com>
2021-08-31 20:11:04 +0900

    @autoApply 학습 사용자가 직접 생성한 class 에 대해서만 사용할 것

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/converter/BookStatusConverter.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/converter/BookStatusConverter.java
@@ -5,7 +5,7 @@ import com.fastcampus.jpa.bookmanager.repository.dto.BookStatus;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
-@Converter
+@Converter(autoApply = true)
 public class BookStatusConverter implements AttributeConverter<BookStatus, Integer> {
 
     @Override
